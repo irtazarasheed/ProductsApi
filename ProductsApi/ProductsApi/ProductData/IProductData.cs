f7f540bcb4e5d4e469d938f22d5dabaad43efb9b
irtazarasheed <irtaza.rasheed@hotmail.com>
2022-05-17 14:00:38 +0200

    Update IProductData.cs

--- ProductsApi/ProductsApi/ProductData/IProductData.cs
+++ ProductsApi/ProductsApi/ProductData/IProductData.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ProductsApi.Models;
+using System;
+using System.Collections.Generic;
 
 namespace ProductsApi.ProductData
 {
